e8581b1fd0abdc1fcae7d39c177795dbf25490ea
chen-christina <christina.chen@breakfast-byte.com>
2025-09-03 13:40:37 +0000

    created starter C# script

---
+++ Interview-Test/Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+class Order
+{
+    // TODO: Define properties (OrderId, CustomerName, TotalAmount, IsProcessed)
+}
+
+class Program
+{
+    static async Task Main()
+    {
+        // TODO: Initialize list of orders with sample data
+
+        // TODO: Print all unprocessed orders
+
+        // TODO: Call ProcessOrdersAsync
+
+        // TODO: Print all processed orders
+    }
+
+    static async Task ProcessOrdersAsync(List<Order> orders)
+    {
+        // TODO: Loop through orders
+        // Simulate processing with Task.Delay(2000)
+        // Mark IsProcessed = true
+    }
+}
