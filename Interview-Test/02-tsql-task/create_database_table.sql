e3c94f33f40db4cab9d7d860019f88b689701c8b
chen-christina <christina.chen@breakfast-byte.com>
2025-09-04 07:41:06 +0000

    quick fixed

--- Interview-Test/02-tsql-task/create_database_table.sql
+++ Interview-Test/02-tsql-task/create_database_table.sql
@@ -18,14 +18,13 @@ CREATE TABLE Employees (
 GO
 
 -- Insert data into Employees
-INSERT INTO Employees (Id, [Name], DepartmentId, Salary) VALUES (
-    1, 'Cyril Darson', 'IT', 4000
-    2, 'Timothy Taylor', 'HR', 5000
-    3, 'Ronny Taylor', 'HR', 4500
-    4, 'Naomi Campbell', 'Operation', 6500
-    5, 'Ernest Yang', 'R&D', 5500
-    6, 'Phenny Yung', 'R&D', 3600
-)
+INSERT INTO Employees (Id, [Name], DepartmentId, Salary) VALUES 
+    (1, 'Cyril Darson', 'IT', 4000),
+    (2, 'Timothy Taylor', 'HR', 5000),
+    (3, 'Ronny Taylor', 'HR', 4500),
+    (4, 'Naomi Campbell', 'Operation', 6500),
+    (5, 'Ernest Yang', 'R&D', 5500),
+    (6, 'Phenny Yung', 'R&D', 3600)
 
 GO
 DROP TABLE IF EXISTS Departments;
@@ -35,12 +34,11 @@ CREATE TABLE Departments(
     )
 GO
 
-INSERT INTO Departments(Id, [Name]) VALUES (
-    51, 'IT' 
-    52, 'HR'
-    53, 'Operation'
-    54, 'R&D'
-    55, 'Facility'
-    56, 'Finance'
-)
+INSERT INTO Departments(Id, [Name]) VALUES
+    (51, 'IT'),
+    (52, 'HR'),
+    (53, 'Operation'),
+    (54, 'R&D'),
+    (55, 'Facility'),
+    (56, 'Finance')
 GO
